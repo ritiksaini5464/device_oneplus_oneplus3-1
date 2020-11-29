03d14e6845da90ec84f212b3f2bd96c0f531a7e5
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-11-29 19:56:54 +0530

    Update octavi_oneplus3.mk

--- octavi_oneplus3.mk
+++ octavi_oneplus3.mk
@@ -21,13 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Tipsy stuff.
-$(call inherit-product, vendor/gzosp/config/common.mk)
-$(call inherit-product, vendor/gzosp/config/gsm.mk)
+$(call inherit-product, vendor/octavi/config/common.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-04-01
 
-PRODUCT_NAME := gzosp_oneplus3
+PRODUCT_NAME := octavi_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MODEL := Oneplus 3
 PRODUCT_MANUFACTURER := OnePlus
@@ -39,5 +38,3 @@ BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:use
 
 TARGET_VENDOR := oneplus
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.gzosp.maintainer="sparx"
